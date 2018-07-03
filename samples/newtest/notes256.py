ac07236cdc36289787f94bc88c8e39a9bb430f36
rmkeezer <rmkeezer@yahoo.com>
2018-07-03 02:28:59 -0400

    moved notes config to separate file to import properly

--- samples/newtest/notes256.py
+++ samples/newtest/notes256.py
@@ -20,6 +20,8 @@ from imageio import imread
 import matplotlib
 import matplotlib.pyplot as plt
 
+from notes_config import NotesConfig
+
 # Root directory of the project
 ROOT_DIR = os.path.abspath("../../")
 
@@ -32,45 +34,6 @@ FULLSET = np.array([fn for fn in os.listdir(ROOT_DIR + '/datasets/midis/2secmask
 np.random.shuffle(FULLSET)
 split = int(len(FULLSET)*0.9)
 TRAINSET, VALSET = FULLSET[:split], FULLSET[split:]
-
-class NotesConfig(Config):
-    """Configuration for training on the toy notes dataset.
-    Derives from the base Config class and overrides values specific
-    to the toy notes dataset.
-    """
-    # Give the configuration a recognizable name
-    NAME = "notes"
-
-    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
-    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 8
-
-    # Number of classes (including background)
-    NUM_CLASSES = 1 + 127  # background + 128 notes
-
-    # Use small images for faster training. Set the limits of the small side
-    # the large side, and that determines the image shape.
-    IMAGE_MIN_DIM = 256
-    IMAGE_MAX_DIM = 256
-
-    # Use smaller anchors because our image and objects are small
-    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
-
-    # Reduce training ROIs per image because the images are small and have
-    # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
-    TRAIN_ROIS_PER_IMAGE = 32
-
-    # Use a small epoch since the data is simple
-    STEPS_PER_EPOCH = 1500
-
-    # use small validation steps since the epoch is small
-    VALIDATION_STEPS = 50
-
-    # If enabled, resizes instance masks to a smaller size to reduce
-    # memory load. Recommended when using high-resolution images.
-    # USE_MINI_MASK = True
-    # MINI_MASK_SHAPE = (32, 32)  # (height, width) of the mini-mask
 
 
 class NotesDataset(utils.Dataset):
---
+++ samples/newtest/notes_config.py
@@ -0,0 +1,53 @@
+
+from mrcnn.config import Config
+import matplotlib.pyplot as plt
+
+class NotesConfig(Config):
+    """Configuration for training on the toy notes dataset.
+    Derives from the base Config class and overrides values specific
+    to the toy notes dataset.
+    """
+    # Give the configuration a recognizable name
+    NAME = "notes"
+
+    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
+    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 8
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 127  # background + 128 notes
+
+    # Use small images for faster training. Set the limits of the small side
+    # the large side, and that determines the image shape.
+    IMAGE_MIN_DIM = 256
+    IMAGE_MAX_DIM = 256
+
+    # Use smaller anchors because our image and objects are small
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
+
+    # Reduce training ROIs per image because the images are small and have
+    # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
+    TRAIN_ROIS_PER_IMAGE = 32
+
+    # Use a small epoch since the data is simple
+    STEPS_PER_EPOCH = 1500
+
+    # use small validation steps since the epoch is small
+    VALIDATION_STEPS = 50
+
+    # If enabled, resizes instance masks to a smaller size to reduce
+    # memory load. Recommended when using high-resolution images.
+    # USE_MINI_MASK = True
+    # MINI_MASK_SHAPE = (32, 32)  # (height, width) of the mini-mask
+
+def get_ax(rows=1, cols=1, size=8):
+    """Return a Matplotlib Axes array to be used in
+    all visualizations in the notebook. Provide a
+    central point to control graph sizes.
+    
+    Change the default size attribute to control the size
+    of rendered images
+    """
+    _, ax = plt.subplots(rows, cols, figsize=(size*cols, size*rows))
+    return ax
